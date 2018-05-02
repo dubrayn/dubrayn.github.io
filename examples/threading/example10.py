65081dbb0ff692dd5ebc5a31810b4ab7844c04e2
noel dubray <noel.dubray@ensiie.fr>
2018-05-02 23:16:47 +0200

    more multiprocessing slides

---
+++ examples/threading/example10.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import logging
+import threading
+import time
+
+logging.basicConfig(level = logging.DEBUG, format = '%(asctime)s.%(msecs)03d [%(levelname)s] (%(threadName)s) %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+l = threading.Lock()
+def worker(n):
+  global l
+  logging.debug("lock.acquire()")
+  l.acquire()
+  logging.debug(" lock acquired !")
+  logging.debug("  lock.acquire() again ?")
+  l.acquire()
+  logging.debug("   lock acquired !")
+  logging.debug("    lock.release()")
+  l.release()
+  logging.debug("     lock released !")
+
+logging.debug("start")
+for i in range(3):
+  threading.Thread(name = 'THREAD-%01d' % (i), target = worker, args = (i,)).start()
---
+++ examples/threading/example9.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import logging
+import threading
+import time
+
+logging.basicConfig(level = logging.DEBUG, format = '%(asctime)s.%(msecs)03d [%(levelname)s] (%(threadName)s) %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+l = threading.Lock()
+a = 0
+def worker(n):
+  global a, l
+  logging.debug("lock.acquire()")
+  l.acquire()
+  logging.debug(" lock acquired !")
+  b = a
+  time.sleep(0.0001)
+  a = b + 1
+  logging.debug("  worker %d: a = %d" % (n, a))
+  logging.debug("   lock.release()")
+  l.release()
+  logging.debug("    lock released !")
+
+logging.debug("start")
+for i in range(3):
+  threading.Thread(name = 'THREAD-%01d' % (i), target = worker, args = (i,)).start()
