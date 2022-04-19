14daecf0a604f0267ead14dd75d177c8c6cffdca
Noel DUBRAY <noel.dubray@cea.fr>
2022-04-19 15:42:59 +0200

    add encode.sh

---
+++ examples/vtk/encode.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# encode a set of .png files into a Firefox-compatible webm file (usable in the template)
+# the files are named 'toto.0000.png', 'toto.0001.png'...
+
+ffmpeg -i toto.%04d.png -c:v libvpx -crf 10 -b:v 1M -c:a libvorbis output.webm
