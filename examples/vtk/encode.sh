ddf45bddd883b08454b0da6e0c8981da2335d80b
Noel DUBRAY <noel.dubray@cea.fr>
2023-04-28 09:36:28 +0200

    cleanup

--- examples/vtk/encode.sh
+++ examples/vtk/encode.sh
@@ -3,4 +3,4 @@
 # encode a set of .png files into a Firefox-compatible webm file (usable in the template)
 # the files are named 'toto.0000.png', 'toto.0001.png'...
 
-ffmpeg -i toto.%04d.png -c:v libvpx -crf 10 -b:v 1M -c:a libvorbis output.webm
+ffmpeg -i output.%04d.png -c:v libvpx -crf 10 -b:v 1M -c:a libvorbis output.webm
