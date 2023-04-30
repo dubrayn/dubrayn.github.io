376a60c61a5ffe6bd844212063c99ee36fc454ab
Noel DUBRAY <noel.dubray@cea.fr>
2023-04-30 12:11:02 +0200

    added explanations in armanpy_pip example

--- examples/armanpy_pip/README.md
+++ examples/armanpy_pip/README.md
@@ -1,3 +1,27 @@
-# Test package
+# Python + SWIG test package
 
-Do not use this package (not yet).
+This simple example shows the correct(?) way to generate and deploy a Python + SWIG package.
+
+# Installation in a venv
+
+Create a `venv` and activate it.
+
+```shell
+python3 -m venv venv
+source venv/bin/activate
+```
+
+Then generate the package and deploy it.
+
+```shell
+python3 -m pip install .
+```
+
+You can finally test the package.
+
+```shell
+cd test_package
+python3 test.py
+```
+
+
--- examples/armanpy_pip/setup.py
+++ examples/armanpy_pip/setup.py
@@ -1,4 +1,12 @@
 from setuptools import setup, Extension
+
+#===============================================================================
+#===============================================================================
+#===============================================================================
+
+# fix an annoying bug with setuptools
+# https://stackoverflow.com/questions/29477298/setup-py-run-build-ext-before-anything-else/48942866#48942866
+
 from setuptools.command.build_py import build_py as _build_py
 
 class build_py(_build_py):
@@ -6,11 +14,19 @@ class build_py(_build_py):
         self.run_command("build_ext")
         return super().run()
 
+#===============================================================================
+#===============================================================================
+#===============================================================================
+
 module1 = Extension('_armanpypsa',
                     include_dirs = ['./armanpy/', 'src/'],
                     libraries = ['m', 'z', 'armadillo'],
                     sources = ['armanpypsa.i', 'src/myclass.cpp'],
                     swig_opts = ["-c++", "-Wall", "-I.", "-I./armanpy/", "-Isrc/"])
+
+#===============================================================================
+#===============================================================================
+#===============================================================================
 
 setup (name = 'armanpypsa',
        py_modules = ['armanpypsa'],
@@ -18,6 +34,6 @@ setup (name = 'armanpypsa',
        description = 'This is a test package',
        install_requires= ['numpy'],
        options = {"build_ext": {"inplace": False}},
-       cmdclass = {'build_py' : build_py},
+       cmdclass = {'build_py' : build_py}, # <= fix for the bug
        ext_modules = [module1])
 
